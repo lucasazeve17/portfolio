807c5fb78ae2b558a20487227c4570b3d9f30951
Lucas Azevedo da Silva <lazevedo@gyramais.com>
2021-03-14 13:11:17 -0300

    adjusts

---
+++ index.js
@@ -0,0 +1,16 @@
+let w = 0
+const arrowLeft = document.querySelector('.arrow')
+const slide = document.querySelector('.slide-body')
+arrowLeft.addEventListener('click',function(){
+    console.log('width', w)
+
+    slide.scroll({
+        top:0,
+        left:w+200,
+        behavior:'smooth'
+    })
+    w = w+200
+    console.log('width', w)
+
+    // alert('alou')
+})
